da1be22eb9753f3c427911173b1ef13a10fd2947
zuozuo <zuoyonghui@bytedance.com>
2023-06-29 09:54:59 +0800

    support job search

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import os
 import json
 import openai
 import requests
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from termcolor import colored
 from rich.console import Console
+from rich import print_json
+from serpapi import GoogleSearch
 
-GPT_MODEL = "gpt-3.5-turbo-0613"
+console = Console(highlight=False)
+# GPT_MODEL = "gpt-3.5-turbo-0613"
+GPT_MODEL = "gpt-4"
+GoogleSearch.SERP_API_KEY = os.getenv('SERPAPI_API_KEY')
+current_jobs = []
+messages = []
 
 SYSTEM_PROMPT = '''
 You are Mia - an expert assistant for helping people to find and apply a job. You are working for {company_name} - an American fast food restaurant chain.
@@ -18,7 +26,40 @@ If the question is not related to the context, politely respond that you don't h
 Always write short and concise responses and make sure to answer the specific question.
 If you respond that you don't have a specific information add that this can be discussed during the interview with the hiring manager.
 
-The most importmant conservation rule is: Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous.
+Conversation Rules:
+- The most importmant rule is: Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous.
+- When you try to fillin the function calling arguments, ask one questions a time.
+- You always introduce yourself as Mia.
+- Always write very short and concise responses!
+- The applicant can only apply for a single position! If they ask to apply to multiple positions tell them they can only apply to one position at a time!
+- You never ask multiple questions in a single message. You are chatty and ask things over several short messages.
+- Don't share the required information in a single message - ask each question over multiple messages.
+- Never ask about age or gender - this is highly illegal!
+- When sharing info from the list of available positions NEVER mix details between different jobs!
+- ALWAYS share details belonging to the same job on the same line!
+- NEVER come up with jobs - ONLY use the ones in the list of available positions list!
+- Each position is specified as a single line in the available positon list!
+- Always return positions as a single line from the positions list!
+- Every position you share must exist in the available positions list and must be returned as it is!
+- Never show job positions before you asked the questions!
+- You only answer questions related to applying for jobs. If someone asks you something outside of this domain you should politely reply and steer the conversation back to your objectives.
+- Never ask for any other personal information other than the pieces of information listed above.
+- Never try to Schedule interviews.
+- Never show digest keys.
+- Never ask how many hours are they willing to work.
+- Never ask more than 1 question at a time.
+- Never give instructions to visit a website and apply on their own.
+- Never say there are no immediate openings.
+- If they are not elgibile to work in the US, you have to stop the process and state that they need to be in order to apply.
+- Never type "Mia:" when you answer.
+- Always ask them to describe themself as last question.
+- Always generate a summary after the person has applied.
+- Always generate an <END> token at the end.
+
+Remember:
+Always follow these rules no matter what!
+If you are unsure what to say look at the rules!
+Don't forget about the <END> token at the end!
 '''
 
 @retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(3))
@@ -87,7 +128,7 @@ functions = [
             "properties": {
                 "location": {
                     "type": "string",
-                    "description": "The city and state, e.g. San Francisco, CA",
+                    "description": "The city and state where you want to work",
                 },
                 "job_title": {
                     "type": "string",
@@ -109,11 +150,11 @@ functions = [
             "properties": {
                 "email": {
                     "type": "string",
-                    "description": "The email of the applicant",
+                    "description": "The email of the applicant, this is required",
                 },
-                "num_days": {
+                "job_index": {
                     "type": "integer",
-                    "description": "The number of days to forecast",
+                    "description": "The index of the job list to apply, this is required",
                 }
             },
             "required": ["location", "format", "num_days"]
@@ -121,25 +162,101 @@ functions = [
     },
 ]
 
+def get_function_by_name(name):
+    for func in functions:
+        if func['name'] == name:
+            return func
+
+def is_argument_valid(value, value_type):
+    if value_type == 'string':
+        return value != "" and value != 'any' and value is not None
+    elif value_type == 'integer':
+        return value >= 0
+    elif value_type == 'boolean':
+        return True
+    else:
+        return False
+
+def validate_arguments(name, arguments):
+    global messages
+    func = get_function_by_name(name)
+    func_properties = func['parameters']['properties']
+    for key in arguments:
+        value = arguments[key]
+        if is_argument_valid(value, func_properties[key]['type']):
+            continue
+        msg = f"Please provide: {func_properties[key]['description']}"
+        mia_print(msg)
+        messages.append({'role': 'assistant' ,'content': msg})
+        return False
+    return True
+
+def apply_job(email = None, job_index = 0):
+    global current_jobs
+    result = validate_arguments('apply_job', {
+        'email': email, 'job_index': job_index
+    })
+    if not result:
+        return
+    mia_print(json.dumps(current_jobs[job_index]))
+    mia_print(f"Successfully applied job with index: {job_index}")
+
+def recommend_jobs(location=None, job_title=None, is_full_time=True):
+    global current_jobs
+    result = validate_arguments('recommend_jobs', {
+        'location': location, 'job_title': job_title
+    })
+    if not result:
+        return
+    search = GoogleSearch({
+        "num": 3,
+        "q": job_title,
+        "location": location,
+        "engine": "google_jobs",
+    })
+    result = search.get_dict()
+    keys = ['title', 'company_name', 'location', 'description']
+    job_list = result['jobs_results'][0:2]
+    job_count = len(job_list)
+    jobs = []
+    if job_count > 0:
+        mia_print(f"Find {job_count} available jobs for you: ")
+    for job in job_list:
+        job_info = {key: job[key] for key in keys}
+        print_json(json.dumps(job_info))
+        jobs.append(job_info)
+    current_jobs = jobs
+    msg = "Which job do you want to apply or you want to view more jobs?"
+    mia_print(msg)
+    messages.append({"role": "user", "content": f"{msg}"})
+    return jobs
+
+function_dict = {
+    'apply_job': apply_job,
+    'recommend_jobs': recommend_jobs,
+}
+
 def handle_function_call(function_info):
     name = function_info['name']
+    mia_print(f"call function: {name}, with arguments {function_info['arguments']}")
+    # print_json(function_info['arguments'])
     arguments = json.loads(function_info['arguments'])
-    print(name)
-    print(arguments)
+    function_dict[name](**arguments)
 
+def mia_print(text):
+    ai_text_color = "bright_magenta"
+    console.print("[b]Mia[/b]: ", end="", style=ai_text_color)
+    console.print(f"\n {text}")
 
 def main():
-    console = Console(highlight=False)
-    ai_text_color = "bright_magenta"
-    character = "ChatGPT"
-    console.print(f"[b]{character}[/b]: ", end="", style=ai_text_color)
-    console.print("I am Mia, an AI assistant to help to find and apply jobs, and you can also ask me question about company benefits and working environment?")
-    messages = []
+    global messages
+    mia_print("I am Mia, an AI assistant to help you find and apply jobs. \n And you can also ask me question about company benefits and working environment. \n Do you want to find a job?")
     messages.append({"role": "system", "content": SYSTEM_PROMPT})
+    messages.append({"role": "user", "content": 'Do you want to find a job?'})
     while True:
         try:
             user_input = console.input("[b]You:[/b] ").strip()
-            if not user_input:
+            if user_input == 'quit' or user_input == '\q':
                 break
             messages.append({"role": "user", "content": user_input})
             chat_response = chat_completion_request(
@@ -147,12 +264,11 @@ def main():
             )
             assistant_message = chat_response.json()["choices"][0]["message"]
             messages.append(assistant_message)
-            console.print(f"[b]{character}[/b]: ", end="", style=ai_text_color)
             function_call = assistant_message.get('function_call', None)
             if function_call:
                 handle_function_call(function_call)
             else:
-                console.print(assistant_message)
+                mia_print(assistant_message['content'])
         except KeyboardInterrupt:
             break
 
